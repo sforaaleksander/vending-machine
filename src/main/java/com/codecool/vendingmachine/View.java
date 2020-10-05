cc87811b04e2556eef9ae25b50260b0a5c8bcebd
sforaaleksander <sforaaleksander@gmail.com>
2020-10-05 14:23:06 +0200

    implement displayProducts() in View.java

--- src/main/java/com/codecool/vendingmachine/View.java
+++ src/main/java/com/codecool/vendingmachine/View.java
@@ -1,16 +1,18 @@
 package com.codecool.vendingmachine;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class View {
-//    Map<Integer, String> map = new HashMap<>();
-//
-//    public void init() {
-//        map.put(1, "Mars");
-//        map.put(2, "Pretzel");
-//        map.put(3, "Spring water");
-//        map.put(4, "Chupa-chups");
-//        map.put(5, "Orange juice");
-//    }
+
+    public void displayProducts(){
+        StringBuilder sb = new StringBuilder();
+        for (Product product : Product.values()) {
+            sb.append(product.id)
+                    .append(". ")
+                    .append(product.name)
+                    .append(": ")
+                    .append(String.format("%.2f", product.price))
+                    .append("\n");
+        }
+        System.out.println(sb.toString());
+    }
 }
