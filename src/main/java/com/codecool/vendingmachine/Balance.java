be9c1c7eb62d187646e389b4dc1a60077b10c212
sforaaleksander <sforaaleksander@gmail.com>
2020-10-05 21:29:47 +0200

    implement Balance.java for managing transaction balance

--- src/main/java/com/codecool/vendingmachine/Balance.java
+++ src/main/java/com/codecool/vendingmachine/Balance.java
@@ -1,2 +1,51 @@
-package com.codecool.vendingmachine;public class Balance {
+package com.codecool.vendingmachine;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Balance {
+    private BigDecimal inserted;
+    private BigDecimal required;
+    private Map<Coin, Integer> coins;
+
+    public Balance() {
+        this.inserted = new BigDecimal("0");
+        this.coins = new HashMap<>();
+        populateMap();
+    }
+
+    public Map<Coin, Integer> getCoins() {
+        return coins;
+    }
+
+    private void populateMap() {
+        coins.put(Coin.FIVE_GROSZ, 10);
+        coins.put(Coin.TEN_GROSZ, 10);
+        coins.put(Coin.TWENTY_GROSZ, 10);
+        coins.put(Coin.FIFTY_GROSZ, 10);
+        coins.put(Coin.ONE_ZLOTY, 10);
+        coins.put(Coin.TWO_ZLOTY, 10);
+        coins.put(Coin.FIVE_ZLOTY, 10);
+    }
+
+    public BigDecimal getInserted() {
+        return inserted;
+    }
+
+    public void insert(BigDecimal inserted) {
+        this.inserted = this.inserted.add(inserted);
+    }
+
+    public BigDecimal getRequired() {
+        return required;
+    }
+
+    public void setRequired(BigDecimal required) {
+        this.required = required;
+    }
+
+    public boolean isPaid(){
+        return inserted.compareTo(required) >= 0;
+    }
 }
