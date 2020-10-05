3592f8985d60fd550c9aa6edd0325554909173dc
sforaaleksander <sforaaleksander@gmail.com>
2020-10-05 21:30:27 +0200

    implement Product.java enum class for storing Products available in machine

--- src/main/java/com/codecool/vendingmachine/Product.java
+++ src/main/java/com/codecool/vendingmachine/Product.java
@@ -1,17 +1,19 @@
 package com.codecool.vendingmachine;
 
+import java.math.BigDecimal;
+
 public enum Product {
-    MARS(1, "Mars", 3.30),
-    PRETZEL(2, "Pretzel", 1.80),
-    SPRING_WATER(3, "Spring water", 2.10),
-    CHUPA_CHUPA(4, "Chupa-chups", 0.90),
-    ORANGE_JUICE(5, "Orange juice", 3.70);
+    MARS(1, "Mars", new BigDecimal("3.30")),
+    PRETZEL(2, "Pretzel", new BigDecimal("1.80")),
+    SPRING_WATER(3, "Spring water", new BigDecimal("2.10")),
+    CHUPA_CHUPA(4, "Chupa-chups", new BigDecimal("0.90")),
+    ORANGE_JUICE(5, "Orange juice", new BigDecimal("3.70"));
 
     int id;
     String name;
-    double price;
+    BigDecimal price;
 
-    Product(int id, String name, double price) {
+    Product(int id, String name, BigDecimal price) {
         this.id = id;
         this.name = name;
         this.price = price;
