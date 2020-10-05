b7bf5536070c9b75777d15eff9a93fed217569fc
sforaaleksander <sforaaleksander@gmail.com>
2020-10-05 21:25:51 +0200

    create Run.java to manage program running

---
+++ src/main/java/com/codecool/vendingmachine/Run.java
@@ -0,0 +1,17 @@
+package com.codecool.vendingmachine;
+
+public class Run {
+    private boolean run;
+
+    public Run() {
+        this.run = true;
+    }
+
+    public boolean isRun() {
+        return run;
+    }
+
+    public void setRun(boolean run) {
+        this.run = run;
+    }
+}
