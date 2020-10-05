235dbcf5b5b3b07dc63c9a4b59fd3790e14c8cab
sforaaleksander <sforaaleksander@gmail.com>
2020-10-05 21:28:34 +0200

    implement Machine.java initializing program

--- src/main/java/com/codecool/vendingmachine/Machine.java
+++ src/main/java/com/codecool/vendingmachine/Machine.java
@@ -1,2 +1,15 @@
-package com.codecool.vendingmachine;public class Machine {
+package com.codecool.vendingmachine;
+
+public class Machine {
+    private Purchase purchase;
+    private Run run;
+
+    public void init(){
+        Run run = new Run();
+        View view = new View();
+        Input input = new Input();
+        while (run.isRun()) {
+            new Purchase(view, input, run).start();
+        }
+    }
 }
