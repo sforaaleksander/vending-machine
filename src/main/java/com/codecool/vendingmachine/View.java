71b5f2075c0e1e0b9891f5f1edf2e42a449c4bd6
sforaaleksander <sforaaleksander@gmail.com>
2020-10-05 21:31:57 +0200

    implement View.java

--- src/main/java/com/codecool/vendingmachine/View.java
+++ src/main/java/com/codecool/vendingmachine/View.java
@@ -2,8 +2,15 @@ package com.codecool.vendingmachine;
 
 
 public class View {
+    String welcomeHeader = "Welcome to the Vending Machine! \nSelect the product by inputting its number:\nOr press zero to exit";
+    String balanceInsertedInfo = "You have inserted: ";
+    String balanceRemainingInfo = "Remaining: ";
 
-    public void displayProducts(){
+    private void displayWelcomeHeader() {
+        System.out.println(welcomeHeader);
+    }
+
+    private void displayProducts() {
         StringBuilder sb = new StringBuilder();
         for (Product product : Product.values()) {
             sb.append(product.id)
@@ -11,8 +18,19 @@ public class View {
                     .append(product.name)
                     .append(": ")
                     .append(String.format("%.2f", product.price))
+                    .append("zl")
                     .append("\n");
         }
         System.out.println(sb.toString());
+    }
+
+    public void displayUserBalance(Balance balance) {
+        System.out.println(balanceInsertedInfo + String.format("%.2f", balance.getInserted()));
+        System.out.println(balanceRemainingInfo + String.format("%.2f", (balance.getRequired().subtract(balance.getInserted()))));
+    }
+
+    public void mainDisplay() {
+        displayWelcomeHeader();
+        displayProducts();
     }
 }
