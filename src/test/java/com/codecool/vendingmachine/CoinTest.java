b49f4d394d103c23256a045596113fe6064c0e70
sforaaleksander <sforaaleksander@gmail.com>
2020-10-05 13:44:33 +0200

    implement further tests for Coin.getCoinByProperties()

--- src/test/java/com/codecool/vendingmachine/CoinTest.java
+++ src/test/java/com/codecool/vendingmachine/CoinTest.java
@@ -1,19 +1,11 @@
 package com.codecool.vendingmachine;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class CoinTest {
-
-//    TEN_GROSZ(16.50, 2.51),
-//    TWENTY_GROSZ(18.50, 3.22),
-//    FIFTY_GROSZ(20.50, 3.94),
-//    ONE_ZLOTY(23.00, 5.00),
-//    TWO_ZLOTY(21.50, 5.21),
-//    FIVE_ZLOTY(24.00, 6.54);
 
     @Test
     public void should_ReturnRightCoin_when_PassCorrectValues(){
@@ -28,7 +20,17 @@ public class CoinTest {
     }
 
     @Test
-    public void should_ThrowException_when_WrongValuesPassed(){
+    public void should_ThrowException_when_PassedBothWrongValues(){
         assertThrows(IllegalArgumentException.class, ()->Coin.getCoinByProperties(1, 2));
+    }
+
+    @Test
+    public void should_ThrowException_when_PassedSingleWrongValue(){
+        assertThrows(IllegalArgumentException.class, ()->Coin.getCoinByProperties(16.50, 0));
+    }
+
+    @Test
+    public void should_ThrowException_when_PassedMissMatchedValues(){
+        assertThrows(IllegalArgumentException.class, ()->Coin.getCoinByProperties(16.50, 3.22));
     }
 }
