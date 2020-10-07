9513850531d4eca3c6adeeb81e6a960830d445a2
sforaaleksander <sforaaleksander@gmail.com>
2020-10-07 18:06:57 +0200

    code cleanup

--- src/main/java/com/codecool/vendingmachine/model/Purchase.java
+++ src/main/java/com/codecool/vendingmachine/model/Purchase.java
@@ -23,14 +23,6 @@ public class Purchase {
         this.run = run;
     }
 
-    public Product getChosenProduct() {
-        return chosenProduct;
-    }
-
-    public void setChosenProduct(Product chosenProduct) {
-        this.chosenProduct = chosenProduct;
-    }
-
     public void start() {
         chooseProduct();
         if(chosenProduct==null) return;
@@ -44,33 +36,13 @@ public class Purchase {
 
     private void giveBackChange() {
         BigDecimal change = balance.calculateChange();
-//        List<Map<Coin, Integer>> possibleChangeCombinations = getPossibleChangeCombinations(change);
-
-//        List<Map<Coin, Integer>> possibleChangeCombinations = getPossibleChangeCombinationsByTree(change);
-//        List<Map<Coin, Integer>> sortedPossibilities = sortCombinations(possibleChangeCombinations);
-//        printCombinations(sortedPossibilities);
-//        if (sortedPossibilities.isEmpty()) {
-//            spitBackInsertedCoins();
-//            view.transactionCanceled();
-//            return;
-//        }
-//        view.printGiveChange(sortedPossibilities.get(0));
         Map<Coin, Integer> changeCoins = getCombinationByTree(change);
         view.printGiveChange(changeCoins);
     }
 
-//    private List<Map<Coin, Integer>> getPossibleChangeCombinationsByTree(BigDecimal change) {
-//        CoinTree coinTree = new CoinTree(change, balance);
-//        coinTree.findAllPaths();
-//        System.out.println("NUMBER OF COMBINATIONS: " + coinTree.getAllPaths().size());
-//        return coinTree.getAllPaths();
-//    }
 
     private Map<Coin, Integer> getCombinationByTree(BigDecimal change) {
         CoinTree coinTree = new CoinTree(change, balance);
-//        coinTree.findAllPaths();
-//        System.out.println("NUMBER OF COMBINATIONS: " + coinTree.getAllPaths().size());
-//        return coinTree.getAllPaths();
         return coinTree.getCombinationByTree(coinTree.getRoot());
     }
 
@@ -81,59 +53,6 @@ public class Purchase {
                         .stream()
                         .mapToInt(Number::intValue)
                         .sum())).collect(Collectors.toList());
-    }
-
-    private void spitBackInsertedCoins() {
-
-    }
-
-
-    private void printCombinations(List<Map<Coin, Integer>> possibleChangeCombinations) {
-        for (Map<Coin, Integer> map : possibleChangeCombinations) {
-            System.out.println("COMBINATION: ");
-            for (Map.Entry<Coin, Integer> entry : map.entrySet()) {
-                System.out.println(entry.getKey() + ": " + entry.getValue());
-            }
-            System.out.println("\n\n\n");
-        }
-    }
-
-    private List<Map<Coin, Integer>> getPossibleChangeCombinations(BigDecimal change) {
-        List<Map<Coin, Integer>> combinations = new ArrayList<>();
-        BigDecimal tempChange;
-        Balance tempBalance;
-        List<Coin> coins = balance.getCoins().keySet()
-                                .stream()
-                                .sorted(Comparator
-                                        .comparingDouble(e->e.value.doubleValue()))
-                                .sorted(Comparator.reverseOrder())
-                                .collect(Collectors.toList());
-        for (Iterator<Coin> it = coins.iterator(); it.hasNext();) {
-            tempChange = new BigDecimal(String.valueOf(change));
-            tempBalance = balance;
-            it.next();
-            Map<Coin, Integer> changeCoins = new HashMap<>();
-            for (Coin coin : coins) {
-                while (tempChange.compareTo(BigDecimal.ZERO) > 0
-                        && coin.value.compareTo(tempChange) <= 0
-                        && tempBalance.getCoins().get(coin) > 0) {
-                    changeCoins.merge(coin, 1, Integer::sum);
-                    tempChange = tempChange.subtract(coin.value);
-                    tempBalance.getCoins().merge(coin, -1, Integer::sum);
-                }
-            }
-            if (!changeCoins.isEmpty() && !combinations.contains(changeCoins) && isCombinationEqualsChange(changeCoins, change)) {
-                combinations.add(changeCoins);
-            }
-            it.remove();
-        }
-        return combinations;
-    }
-
-    private boolean isCombinationEqualsChange(Map<Coin, Integer> changeCoins, BigDecimal change) {
-        double totalFromCombination = changeCoins.entrySet()
-                .stream().mapToDouble(e->e.getKey().value.multiply(new BigDecimal(e.getValue().toString())).doubleValue()).sum();
-        return change.doubleValue() == totalFromCombination;
     }
 
     private void collectCoins() {
--- src/main/java/com/codecool/vendingmachine/model/tree/CoinTree.java
+++ src/main/java/com/codecool/vendingmachine/model/tree/CoinTree.java
@@ -22,34 +22,7 @@ public class CoinTree {
         coinList = Stream.of(Coin.values()).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         this.allPaths = new ArrayList<>();
         this.balance = balance;
-//        createTree(root);
     }
-
-    public List<Map<Coin, Integer>> getAllPaths() {
-        return allPaths;
-    }
-
-    public void findAllPaths() {
-        traverse(root);
-    }
-
-    private void traverse(Node node) {
-        if (node.getRemainingChange().compareTo(BigDecimal.ZERO) == 0 && !allPaths.contains(node.getCollectedCoins())) {
-            allPaths.add(node.getCollectedCoins());
-        }
-        for (Node child : node.getChildren()) {
-            traverse(child);
-        }
-    }
-
-//    private void createTree(Node node) {
-//        for (Coin coin : coinList) {
-//            if (coin.value.compareTo(node.getRemainingChange()) <= 0) {
-//                Node childNode = node.insertNode(coin);
-//                createTree(childNode);
-//            }
-//        }
-//    }
 
     public Map<Coin, Integer> getCombinationByTree(Node node) {
         for (Coin coin : coinList) {
